43e7dc425b76e579f3e2f2e8b33b00340875ed0b
Stefan Adams <Stefan.Adams@keystone-it.com>
2014-10-15 18:09:05 -0500

    ...

--- lib/Comics/Comic.pm
+++ lib/Comics/Comic.pm
@@ -11,25 +11,45 @@ use Comics::Strips;
 has name => 'Sample';
 has filename => sub { shift->basepackagename };
 has link => 'http://samp.le';
+has days => sub { [] };
 has strips => sub { Comics::Strips->new(comic => shift) };
-sub download { warn "Not yet implemented" }
+has agent => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/31.0.1650.63 Chrome/31.0.1650.63 Safari/537.36';
+has referer => 'http://www.google.com';
+has headers => sub { {'User-Agent' => $_[0]->agent, 'Referer' => $_[0]->referer} };
 
-has _ua => sub { Mojo::UserAgent->new };
-has _types => sub { Mojolicious::Types->new };
-
-sub _download {
-  my ($self, $strip, $dom) = @_;
-
-  #my $tx = $self->_ua->get($self->_ua->get($self->_resolved_link($strip->date))->res->dom->$dom->first);
-  #$strip->ext($self->_types->detect($tx->res->headers->content_type)->[0]);
-  #$tx->res->content->asset->move_to($strip->abs_path);
-  $strip->exists;
+sub datedlink {
+  my ($self, $date) = @_;
+  my $link = $self->can('datelink') ? $self->datelink : $self->link;
+  return $link unless $date;
+  $link = Date::Simple::D8->new($date)->format($link);
+  $link =~ s/ //g;
+  $link;
 }
 
-sub _resolved_link {
-  my ($self, $date) = @_;
-  return $self->link unless $date;
-  Date::Simple::D8->new($date)->format($self->link);
+has ua => sub { Mojo::UserAgent->new };
+has types => sub { Mojolicious::Types->new };
+
+sub download {
+  my ($self, $strip) = @_;
+  if ( @{$self->days} ) {
+    return 0 unless grep { $_ eq $strip->date->format('%a') } @{$self->days};
+  }
+  warn sprintf "Getting %s\n", $self->datedlink($strip->date);
+  my $tx;# = $self->ua->get($self->ua->get($self->datedlink($strip->date) => $self->headers)->dom->$dom->first => $self->headers);
+  $self->save($strip, $tx);
+}
+
+sub save {
+  my ($self, $strip, $tx) = @_;
+
+  if ( $tx ) {
+    $strip->ext($self->types->detect($tx->res->headers->content_type)->[0]);
+    $tx->res->content->asset->move_to($strip->abs_path);
+  } else {
+    warn "No \$tx found\n";
+  }
+
+  $strip->exists;
 }
 
 1;
